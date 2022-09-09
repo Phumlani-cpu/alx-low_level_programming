6fb1f5cf8126772cec4e7db19bb4e837c626e95a
Phumlani-cpu <phumlanivermaak@gmail.com>
2022-09-09 07:44:51 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
+
 /**
- * maim-main block
- * Return: 0
- * */
-int main (void)
+ * main-Prints a text
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
 {
-	printf ("with proper grammar,but the outcome is a piece of art,\");
+	printf("with proper grammar,but the outcome is a piece of art,\n");
 	return (0);
 }
